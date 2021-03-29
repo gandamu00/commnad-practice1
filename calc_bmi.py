effb95b59294f3955071f067de4ec6fb12e74899
miyahara <y.miyahara.sys20@morijyobi.ac.jp>
2021-03-29 12:05:31 +0900

    入力インターフェイスの修正

---
+++ calc_bmi.py
@@ -0,0 +1,4 @@
+def get_area(kg,height):
+    area = kg / (height * height)
+    return area
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import calc_bmi
+
+print("BMI計算アプリ")
+h = int(input("身長:"))
+k = int(input("体重:"))
+print(f"BMI:{calc_bmi.get_area(h,k)}")
+
+
