248bb7c8dc11bcf3ed92660396e858e888b5419d
miyahara <y.miyahara.sys20@morijyobi.ac.jp>
2021-03-29 12:30:42 +0900

    間違ったコミットメッセージ

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import calc_bmi
 
 print("BMI計算アプリ")
-h = int(input("身長:"))
-k = int(input("体重:"))
+h = int(input("身長:170"))
+k = int(input("体重:56"))
+for counter in range(2):
+    print(conuter)
 print(f"BMI:{calc_bmi.get_area(h,k)}")
+
 
 
